a361411eb75fa29b1d86322a8a6c5f9f2d01aa6d
EudrinoDev <justxirdx+2@gmail.com>
2022-08-18 19:10:23 +0000

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,161 @@
+import { searchMovie, getMovieData } from "../utils/Fushaar.js";
+import { decode } from "html-entities";
+import {
+  Client,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActivityType,
+} from "discord.js";
+
+const EMOJIS = {
+  1: "1️⃣",
+  2: "2️⃣",
+  3: "3️⃣",
+  4: "4️⃣",
+  5: "5️⃣",
+};
+const ALIAS = {
+  1: "one",
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+};
+
+const botClient = new Client({
+  intents: [],
+  presence: {
+    status: "dnd",
+    activities: [
+      {
+        type: ActivityType.Listening,
+        name: "Sweety's Orders",
+      },
+    ],
+  },
+});
+
+botClient
+  .on("interactionCreate", async (interaction) => {
+    if (interaction.isButton()) {
+      const args = interaction.customId.split("-");
+      switch (args[0]) {
+        case "select":
+          const movieUrl = decodeURIComponent(args.slice(1).join("-"));
+          interaction.deferUpdate();
+          await botClient.channels.fetch(interaction.channelId);
+          const movieData = await getMovieData(movieUrl);
+          interaction.message.edit({
+            embeds: [
+              new EmbedBuilder()
+                .setAuthor({
+                  name: botClient.user.username,
+                  iconURL: botClient.user.displayAvatarURL({ dynamic: true }),
+                })
+                .setTitle(movieData.title)
+                .setDescription(
+                  `>>> **\`\`\`fix\n${decode(movieData.description)}\`\`\`**`
+                )
+                .addFields([
+                  {
+                    name: "About 🤔",
+                    value: `>>> **Release Date: \`${movieData.year}\`\nLength: \`${movieData.length}m\`\nPG: \`${movieData.pg}\`\nRating: \`${movieData.rating}\`**`,
+                    inline: true,
+                  },
+                  {
+                    name: "Categories 🗂️",
+                    value: movieData.genres
+                      .map(
+                        (category) =>
+                          `> **[${category.title}](${category.url})**`
+                      )
+                      .join("\n"),
+                    inline: true,
+                  },
+                ])
+                .setURL(movieUrl)
+                .setColor("Green")
+                .setThumbnail(movieData.banner),
+            ],
+            components: [
+              new ActionRowBuilder().setComponents([
+                new ButtonBuilder()
+                  .setEmoji("📺")
+                  .setLabel("Watch")
+                  .setStyle(ButtonStyle.Link)
+                  .setURL(
+                    `https://www.fushaar.com/assets/themes/fushaarV5/player.php?id=${movieData.watch}`
+                  ),
+              ]),
+            ],
+          });
+          break;
+      }
+    }
+    if (interaction.isCommand()) {
+      switch (interaction.commandName) {
+        case "movie":
+          {
+            const name = interaction.options.getString("name", true);
+            const movies = (await searchMovie(name)).slice(0, 3);
+            interaction.reply({
+              content: interaction.user.toString(),
+              embeds: movies.map((movie, index) =>
+                new EmbedBuilder()
+                  .setAuthor({
+                    name: decode(movie.title),
+                    iconURL: `https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/322/keycap-digit-${
+                      ALIAS[index + 1]
+                    }_${31 + index}-fe0f-20e3.png`,
+                    url: movie.url,
+                  })
+                  .setThumbnail(movie.art)
+                  .addFields([
+                    {
+                      name: "Categories 🗂️",
+                      value: movie.genres
+                        .map(
+                          (category) =>
+                            `> **[${category.title}](${category.url})**`
+                        )
+                        .join("\n"),
+                      inline: true,
+                    },
+                    {
+                      name: "More",
+                      value: `>>> **PG: ${movie.pg}\nReleased: ${
+                        movie.year
+                      }\nTranslated: ${movie.translated ? "✅" : "❌"}**`,
+                      inline: true,
+                    },
+                  ])
+                  .setColor("Green")
+              ),
+              components: [
+                new ActionRowBuilder()
+                  .addComponents(
+                    movies.map((movie, index) =>
+                      new ButtonBuilder()
+                        .setCustomId("select-" + encodeURIComponent(movie.url))
+                        .setEmoji(EMOJIS[index + 1])
+                        .setStyle(ButtonStyle.Secondary)
+                    )
+                  )
+                  .addComponents([
+                    new ButtonBuilder()
+                      .setLabel("More")
+                      .setStyle(ButtonStyle.Link)
+                      .setURL(
+                        `https://www.fushaar.com/?s=${encodeURIComponent(name)}`
+                      ),
+                  ]),
+              ],
+            });
+          }
+          break;
+      }
+    }
+  })
+  .login(process.env.TOKEN);
---
+++ utils/Fushaar.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+export async function searchMovie(query) {
+  const { data } = await axios.get(
+    `https://www.fushaar.com/?s=${encodeURIComponent(query)}`
+  );
+  return data
+    .split('article class="poster"')
+    .slice(1)
+    .map((movie) => ({
+      title: movie.split("<h3>")[1].split("</h3>")[0],
+      url: movie.split('href="')[1].split('"')[0],
+      art: movie.split('data-lazy-src="')[1].split('"')[0],
+      ar_title: movie.split("<h4>")[1].split("</h4>")[0],
+      genres: movie
+        .split('itemprop="genre"')
+        .slice(1)
+        .map((genre) => ({
+          url: genre.split('href="')[1].split('"')[0],
+          title: genre.split(">")[1].split("</a")[0],
+        })),
+      year: movie.split('year">')[1].split("</li>")[0],
+      pg: movie.split("li")[3].split("</li")[0].slice(1, -2),
+      translated: movie.split("li")[5].split("</li")[0].slice(1, -2) == "مترجم",
+    }));
+}
+
+export async function getMovieData(url) {
+  const { data } = await axios.get(url);
+  return {
+    title: data.split("<span>")[1].split("</span>")[0],
+    ar_title: data.split('<h2 class="cemter">')[1].split("</h2>")[0],
+    year: data.split('<span class="yearz">(')[1].split(")</span>")[0],
+    genres: data
+      .split('<div class="gerne">')[1]
+      .split('itemprop="genre"')
+      .slice(1)
+      .map((genre) => ({
+        url: genre.split('href="')[1].split('"')[0],
+        title: genre.split('">')[1].split("</a>")[0],
+      })),
+    length: data.split('info-boxy">')[1].split("<span")[0],
+    pg: data.split('info-boxy">')[2].split("<span")[0],
+    quality: data.split('info-boxy">')[3].split("<span")[0],
+    banner: data.split("background: url(")[1].split(")")[0],
+    art: data
+      .split('<figure class="poster">')[1]
+      .split('data-lazy-src="')[1]
+      .split('"')[0],
+    rating: data.split('<div class="imdb">')[1].split("</div>")[0],
+    description: data
+      .split('<div class="postz" >')[1]
+      .split("<p>")[1]
+      .split("</p>")[0],
+    watch: data
+      .split(
+        'data-lazy-src="https://www.fushaar.com/assets/themes/fushaarV5/player.php?id='
+      )[1]
+      .split('"')[0],
+  };
+}
